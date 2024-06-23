725d098b20a3b1963753f0ff84f051847a000090
Aditya Lohar <77808500+AdityaLohar@users.noreply.github.com>
2024-06-24 04:02:59 +0530

    add files

---
+++ README.md
@@ -0,0 +1,22 @@
+### This web application allows users to perform mathematical operations on a number within the range of 0 to 150. It includes features such as adding, subtracting, displaying a progress bar, and implementing undo/redo functionality.
+
+## Features
+1. Mathematical Operations
+- Clicking the Add button increments the number by 1.
+- Clicking the Subtract button decrements the number by 1.
+- The number is constrained between 0 and 150 (inclusive).
+
+2. Progress Bar
+- A progress bar visually represents the current number.
+- The progress bar updates as the number changes.
+
+3. Undo/Redo Functionality
+- Users can undo previous operations (up to the initial state).
+- Redo functionality allows users to redo operations that were undone.
+
+
+## Technology Used: 
+- This application is built using React.js for the frontend.
+- Styling: Basic CSS is applied.
+- State Management: React useState hooks are used to manage the application state.
+- Animation: CSS transitions are applied to the progress bar for smooth updates.
---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import { useState } from 'react';
+import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [history, setHistory] = useState([0]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [showError, setShowError] = useState(false)
+
+  const handleAdd = () => {
+    if (count < 150) {
+      const newCount = count + 1;
+      const newHistory = [...history.slice(0, currentIndex + 1), newCount];
+      setHistory(newHistory);
+      setCount(newCount);
+      setCurrentIndex(currentIndex + 1);
+      console.log(history, newCount)
+    }
+    else {
+      if (showError === false) {
+        setShowError(true)
+        setTimeout(() => setShowError(false), 4000)
+        toast.error("Number cannot be greater than 150", {
+          position: 'bottom-center',
+          theme: "dark",
+          autoClose: 3000,
+        })
+      }
+    }
+  };
+  
+  const handleSubtract = () => {
+    if (count > 0) {
+      const newCount = count - 1;
+      const newHistory = [...history.slice(0, currentIndex + 1), newCount];
+      setHistory(newHistory);
+      setCount(newCount);
+      setCurrentIndex(currentIndex + 1);
+    }
+    else {
+      if (showError === false) {
+        setShowError(true)
+        setTimeout(() => setShowError(false), 4000)
+        toast.error("Number cannot be less than 0", {
+          position: 'bottom-center',
+          theme: "dark",
+          autoClose: 3000,
+        })
+      }
+    }
+  };
+
+  const handleUndo = () => {
+    if (currentIndex > 0) {
+      setCount(history[currentIndex - 1]);
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+
+  const handleRedo = () => {
+    if (currentIndex < history.length - 1) {
+      setCount(history[currentIndex + 1]);
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
+  return (
+    <div>
+      <div className='buttons'>
+        <button onClick={handleAdd}>Add</button>
+        <button onClick={handleSubtract}>Subtract</button>
+
+        <button onClick={handleUndo} disabled={currentIndex === 0}>Undo</button>
+        <button onClick={handleRedo} disabled={currentIndex === history.length - 1}>Redo</button>
+      </div>
+
+      <div className='count'>Count: {count}</div>
+
+      <div className='results'>
+        <div className="progress-bar">
+          <div className="progress1" style={{ width: `${(count / 150) * 100}%` }} ></div>
+        </div>
+
+        <p> {Math.round((count / 150) * 100)}% </p>
+      </div>
+
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
